63aa5be38c2a1e36cb05201bfcfcee62ab15174c
Matt Snook <matt.snook@adhocteam.us>
2020-10-30 19:14:41 -0400

    DPC-818: Create .jekyll-cache to avoid permission issues

--- scripts/build_and_test.sh
+++ scripts/build_and_test.sh
@@ -14,7 +14,7 @@ build() {
 test() {
     trap docker_cleanup EXIT
     docker-compose run -u "$(id -u "${USER}")":"$(id -g "${USER}")" --publish 4000:4000 --rm --entrypoint "bundle exec jekyll serve -H 0.0.0.0" --name static_site -d static_site
-    sleep 40
+    sleep 20
     docker logs static_site
     curl localhost:4000 | grep "Join the Production Pilot Onboarding Queue"
 }
