ccbeec4803daed74016aae9f73797e53d8923aa8
Matt Snook <matt.snook@adhocteam.us>
2020-10-30 19:07:45 -0400

    DPC-818: Log output

--- scripts/build_and_test.sh
+++ scripts/build_and_test.sh
@@ -13,8 +13,9 @@ build() {
 
 test() {
     trap docker_cleanup EXIT
-    docker-compose run -u "$(id -u "${USER}")":"$(id -g "${USER}")" --publish 4000:4000 --rm --entrypoint "bundle exec jekyll serve -H 0.0.0.0" -d static_site
-    sleep 20
+    docker-compose run -u "$(id -u "${USER}")":"$(id -g "${USER}")" --publish 4000:4000 --rm --entrypoint "bundle exec jekyll serve -H 0.0.0.0" --name static_site -d static_site
+    sleep 40
+    docker logs static_site
     curl localhost:4000 | grep "Join the Production Pilot Onboarding Queue"
 }
 
