30fa8cd244bc6d41adde51ce6ab06fb73bdc441c
jdettmannnava <145699825+jdettmannnava@users.noreply.github.com>
2024-03-15 16:04:49 -0400

    navbar listeners

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -10,7 +10,7 @@ var close_mobile_nav_button = document.getElementById("topnav__close-button"),
 function removeElementsByClass(className){
   var elements = document.getElementsByClassName(className);
   while(elements.length > 0){
-      elements[0].parentNode.removeChild(elements[0]);
+    elements[0].parentNode.removeChild(elements[0]);
   }
 }
 
@@ -45,3 +45,28 @@ if(typeof(open_mobile_nav_button) != 'undefined' && open_mobile_nav_button != nu
     trapFocus(mobile_nav);
   })
 }
+
+const site_logo =  document.querySelector("a.site-logo");
+
+site_logo.addEventListener("click", function(e) {
+  utag.link({
+    "event_name": "header_clicked",
+    "event_type": "ui interaction",
+    "text": site_logo.text,
+    "link_url": site_logo.href,
+    "category": "page header"
+  });
+});
+
+
+for (anchor of document.getElementById("static-site-links").getElementsByTagName("a")) {
+  anchor.addEventListener("click", function(e) {
+    utag.link({
+      "event_name": "internal_link_clicked",
+      "text": anchor.text,
+      "link_url": anchor.href,
+      "link_type": "link_other"
+    });
+  });
+}
+
