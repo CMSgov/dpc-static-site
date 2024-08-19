a4b86ecef38dbf26b19adbfa374cf0f26f3ed828
Ashley Weaver <134093673+ashley-weaver@users.noreply.github.com>
2024-08-19 15:47:52 -0400

    Update Docker commands (#126)
    ## 🎫 Ticket
    
    https://jira.cms.gov/browse/...
    
    ## 🛠 Changes
    
    <!-- What was added, updated, or removed in this PR? -->
    
    ## ℹ️ Context
    
    <!-- Why were these changes made? Add background context suitable for a
    non-technical audience. -->
    
    <!-- If any of the following security implications apply, this PR must
    not be merged without Stephen Walter's approval. Explain in this section
    and add @SJWalter11 as a reviewer.
      - Adds a new software dependency or dependencies.
      - Modifies or invalidates one or more of our security controls.
      - Stores or transmits data that was not stored or transmitted before.
    - Requires additional review of security implications for other reasons.
    -->
    
    ## 🧪 Validation
    
    <!-- How were the changes verified? Did you fully test the acceptance
    criteria in the ticket? Provide reproducible testing instructions and
    screenshots if applicable. -->

--- scripts/build_and_test.sh
+++ scripts/build_and_test.sh
@@ -4,16 +4,16 @@ set -eo pipefail
 
 docker_cleanup()
 {
-    docker-compose down
+    docker compose down
 }
 
 build() {
-    docker-compose -f docker-compose.yml build static_site
+    docker compose -f docker-compose.yml build static_site
 }
 
 test() {
     trap docker_cleanup EXIT
-    docker-compose run -u "$(id -u "${USER}")":"$(id -g "${USER}")" --publish 4000:4000 --rm --entrypoint "bundle exec jekyll serve -H 0.0.0.0" --name static_site -d static_site
+    docker compose run -u "$(id -u "${USER}")":"$(id -g "${USER}")" --publish 4000:4000 --rm --entrypoint "bundle exec jekyll serve -H 0.0.0.0" --name static_site -d static_site
     sleep 20
     docker logs static_site
     curl localhost:4000 | grep "Update: what we're working on" || { echo "ERROR: Updates page not found"; exit 1; }
