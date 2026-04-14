e763476e8765fa8cf6b9ff4f5b467b91de95309b
Ashley Weaver <ashleyweaver@navapbc.com>
2026-04-14 13:49:16 -0400

    Update build_and_test.sh

--- scripts/build_and_test.sh
+++ scripts/build_and_test.sh
@@ -29,7 +29,7 @@ test() {
     TARGETS_TO_SCAN="${TARGETS_TO_SCAN} ${TARGET_TEST_ENV}/docsV1"
     TARGETS_TO_SCAN="${TARGETS_TO_SCAN} ${TARGET_TEST_ENV}/docsV2"
     TARGETS_TO_SCAN="${TARGETS_TO_SCAN} ${TARGET_TEST_ENV}/updates"
-    docker run --platform linux/arm64 --add-host=host.docker.internal:host-gateway --init --rm --cap-add=SYS_ADMIN ghcr.io/puppeteer/puppeteer:24.40.0 ${TARGETS_TO_SCAN}
+    docker run --add-host=host.docker.internal:host-gateway --init --rm --cap-add=SYS_ADMIN eternalfree/puppeteer:23.11.1-linux-arm64 ${TARGETS_TO_SCAN}
 }
 
 main() {
